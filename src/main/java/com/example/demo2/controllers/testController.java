2cad097f6ee931444cad1bd72e5a83d5927f2a2b
eshwar1111 <eshwarkumar1111@gmail.com>
2025-09-24 15:45:17 +0530

    commit 1

---
+++ src/main/java/com/example/demo2/controllers/testController.java
@@ -0,0 +1,16 @@
+package com.example.demo2.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@RestController
+public class testController {
+
+    @GetMapping("/test")
+    public String getMethodName() {
+        return "working";
+    }
+    
+    
+}
