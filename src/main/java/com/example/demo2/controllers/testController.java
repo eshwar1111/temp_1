3ef556db1e8387391cf4245197c22ac1ad753603
eshwar1111 <eshwarkumar1111@gmail.com>
2025-09-24 17:02:44 +0530

    t3

--- src/main/java/com/example/demo2/controllers/testController.java
+++ src/main/java/com/example/demo2/controllers/testController.java
@@ -10,7 +10,7 @@ public class testController {
 
     @GetMapping("/test")
     public String getMethodName() {
-        return "workingt1-t2";
+        return "workingt1-t2-t3";
     }
 
     @GetMapping("/test2")
